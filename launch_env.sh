76ce0c1c949ebb4fc8f4d739369364923268e48b
leomonde <129780808+leomonde@users.noreply.github.com>
2023-07-11 16:14:13 -0300

    Add clock sync from log and log auto clean
    Add clock sync from log and log auto clean

--- launch_env.sh
+++ launch_env.sh
@@ -19,3 +19,34 @@ if [ -z "$PASSIVE" ]; then
 fi
 
 export STAGING_ROOT="/data/safe_staging"
+
+# Set system date equal to last date of recording, in case of no network to sync time
+
+ls /data/media/0/realdata/ > /tmp/rec_list.tmp
+
+# Considering at least 3 directory because of /boot and /crash
+if [ `wc -l < /tmp/rec_list.tmp` -ge 3 ]
+  then
+   last_day=`cat /tmp/rec_list.tmp | tail -3 | head -1 | awk -F"--" '{print $1}'`
+   last_time=`cat /tmp/rec_list.tmp | tail -3 | head -1 | awk -F"--" '{print $2}' | sed -r 's/-/:/g'`
+   date --set $last_day" "$last_time
+fi
+
+# Clean oldest recording day if disk space is less than 20GB
+
+df -h | grep "/data" | awk '{print $4}' > /tmp/part_space.tmp
+space=`sed -e 's/G//' /tmp/part_space.tmp`
+# rm /tmp/part_space.tmp
+
+if [ $space -lt 20 ]
+then
+ old_day=`sed -n '1p' /tmp/rec_list.tmp | awk -F"--" '{print $1}'`
+ for file in `ls /data/media/0/realdata/ | grep $old_day`
+ do
+  rm -rf "/data/media/0/realdata/"$file
+ done
+ echo $old_day >> /tmp/last_clean.tmp
+ df -h | grep "/data" | awk '{print $4}' > /tmp/part_space.tmp
+fi
+
+rm /tmp/rec_list.tmp
