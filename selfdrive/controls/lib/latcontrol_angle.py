5c445605df73545b2c97ce116bde8882e5b0559f
leomonde <129780808+leomonde@users.noreply.github.com>
2023-07-19 15:05:46 -0300

    vp adjustment

--- selfdrive/controls/lib/latcontrol_angle.py
+++ selfdrive/controls/lib/latcontrol_angle.py
@@ -3,7 +3,7 @@ import math
 from cereal import log
 from selfdrive.controls.lib.latcontrol import LatControl, MIN_STEER_SPEED
 
-STEER_ANGLE_SATURATION_THRESHOLD = 2.5  # Degrees
+STEER_ANGLE_SATURATION_THRESHOLD = 10  # Degrees
 
 
 class LatControlAngle(LatControl):
